6f9a54e0283745b5c3eb62c4a5a598d89d5b93ae
John Bielejeski <jmbielejeski@gmail.com>
2021-03-16 11:13:22 -0700

    added basic Material UI to friendList

--- src/components/FriendsList/FriendsList.jsx
+++ src/components/FriendsList/FriendsList.jsx
@@ -2,7 +2,30 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
+// Material UI imports
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
 function FriendsList() {
+  // Material UI
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    control: {
+      padding: theme.spacing(2),
+      margin: 'auto',
+    },
+  }));
+  
+  const classes = useStyles();
+  // End Material UI
 
   const history = useHistory();
   const dispatch = useDispatch();
@@ -43,26 +66,44 @@ function FriendsList() {
   }
 
   return (
-    <div>
-      <h2>List of friends!</h2>
-      <ul>
-        {friendList.map(friend => {
-          return(
-            <li key={friend.id} onClick={() => handleClick(friend.id)}>{friend.username}</li>
-          )
-        })}
-      </ul>
-      <form onSubmit={getSearchResults}>
-        <input 
-          type="text"
-          name="search"
-          placeholder="Search for friend"
-          value={friendSearch}
-          onChange={(event) => setFriendSearch(event.target.value)}
-        />  
-        <button>Search</button>
-      </form>
-    </div>
+    <Grid 
+      container 
+      className={classes.root} 
+      spacing={2}
+      alignItems="center"
+      direction="column"
+    >
+      <Grid item xs={12}>
+        List of friends!
+      </Grid>
+      <Grid item xs={12}>
+        <Grid item xs={12}>
+          {friendList.map(friend => {
+            return(
+              <li key={friend.id} onClick={() => handleClick(friend.id)}>{friend.username}</li>
+            )
+          })}
+        </Grid>
+        <FormControl component="fieldset">
+        <FormLabel component="legend">Search for a friend to add</FormLabel>          <input 
+            type="text"
+            name="search"
+            placeholder="Search for friend"
+            value={friendSearch}
+            onChange={(event) => setFriendSearch(event.target.value)}
+          />  
+          <Button 
+            type="submit" 
+            variant="contained" 
+            color="primary" 
+            className={classes.button} 
+            onClick={getSearchResults}
+          >
+            Search
+          </Button>
+        </FormControl>
+      </Grid>
+    </Grid>
   )
 }
 
