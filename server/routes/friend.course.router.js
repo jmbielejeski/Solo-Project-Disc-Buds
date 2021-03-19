4a932672c618dd7d82d976ae2d33ed52228034b3
John Bielejeski <jmbielejeski@gmail.com>
2021-03-18 19:44:01 -0700

    setup router query to grab selected friends course history

--- server/routes/friend.course.router.js
+++ server/routes/friend.course.router.js
@@ -6,7 +6,25 @@ const router = express.Router();
 router.post('/', (req, res) => {
   console.log('in friendCourse router', req.body);
 
-  
+  let friendId = req.body.friendId;
+  let courseId = req.body.courseId;
+  let holeCount = req.body.holeCount;
+
+  const queryText = `
+  SELECT * FROM "course_history"
+  WHERE user_id = $1 AND course_id = $2
+  LIMIT $3
+  `
+
+  pool.query(queryText, [friendId, courseId, holeCount])
+    .then((dbRes) => {
+      console.log('courseHistory', dbRes.rows);
+      res.send(dbRes.rows);
+    })
+    .catch((error) => {
+      console.log('error getting friend match history', error);
+      res.sendStatus(500);
+    })
 
 });
 
