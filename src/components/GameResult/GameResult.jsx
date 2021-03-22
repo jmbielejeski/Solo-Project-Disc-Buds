5b820071d64f9eb6d1f02f67385eb84814f7bbbb
John Bielejeski <jmbielejeski@gmail.com>
2021-03-22 09:48:25 -0700

    added additional styling and added icons to indrease or decrease score

--- src/components/GameResult/GameResult.jsx
+++ src/components/GameResult/GameResult.jsx
@@ -2,9 +2,31 @@ import { SquareFootOutlined } from '@material-ui/icons';
 import React from 'react';
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import Swal from 'sweetalert2'
 
+// Material UI imports
+import Grid from '@material-ui/core/Grid';
+import {Button} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
 function GameResult() {
+
+  // Material UI
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    control: {
+      padding: theme.spacing(2),
+      margin: 'auto',
+    },
+  }));
+  
+  const classes = useStyles();
+  // End Material UI
+  
+  const history = useHistory();
 
   const matchResults = useSelector(store => store.matchResultsReducer);
 
@@ -34,23 +56,49 @@ function GameResult() {
 
   if (matchResults.friend === undefined) {
     return (
-      <div>
-        <h2>Game Results!</h2>
-        <h4>your score: {matchResults.yourScore}</h4>
-        <Link className="navLink" to='/homePage'>Home page</Link>
-  
-      </div>
+      <Grid
+        container 
+        className={classes.root} 
+        spacing={2}
+        alignItems="center"
+        direction="column"
+      >
+        <Grid item xs={12}>Game Results!</Grid>
+        <Grid item xs={12}>your score: {matchResults.yourScore}</Grid>
+          <Button 
+            type="submit" 
+            variant="contained" 
+            color="primary" 
+            className={classes.button}           
+            onClick={() => {history.push('/')}}
+          >
+            Home page
+          </Button>
+        </Grid>
     )
   } else {
 
   return (
-    <div>
-      <h2>Game Results!</h2>
-      <h4>your score: {matchResults.yourScore}</h4>
-      <h4>{matchResults.friend}'s score: {matchResults.friendScore}</h4>
-      <Link className="navLink" to='/homePage'>Home page</Link>
-
-    </div>
+    <Grid        
+      container 
+      className={classes.root} 
+      spacing={2}
+      alignItems="center"
+      direction="column"
+    >
+      <Grid item xs={12}>Game Results!</Grid>
+      <Grid item xs={12}>Your score: {matchResults.yourScore}</Grid>
+      <Grid item xs={12}>{matchResults.friend}'s score: {matchResults.friendScore}</Grid>
+      <Button 
+        type="submit" 
+        variant="contained" 
+        color="primary" 
+        className={classes.button}           
+        onClick={() => {history.push('/')}}
+      >
+        Home page
+      </Button>
+    </Grid>
   )
   }
 }
