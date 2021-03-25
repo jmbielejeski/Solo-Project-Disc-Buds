d64556751888c9abea30fa717b6547c193d547b4
John Bielejeski <jmbielejeski@gmail.com>
2021-03-25 08:14:19 -0700

    added SQL code to database file

--- database.sql
+++ database.sql
@@ -4,7 +4,32 @@
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+  "id" SERIAL PRIMARY KEY,
+  "username" VARCHAR (80) UNIQUE NOT NULL,
+  "password" VARCHAR (1000) NOT NULL
+);
+
+-- Create our friends table which is a one way relationship
+CREATE TABLE "friends" (
+  "id" SERIAL PRIMARY KEY,
+  "user_one" INT REFERENCES "user",
+  "user_two" INT REFERENCES "user"
+); 
+
+-- course history table
+CREATE TABLE "course_history" (
+	"id" SERIAL PRIMARY KEY,
+	"course_id" INT,
+	"user_id" INT REFERENCES "user",
+	"hole_score" INT,
+	"hole_index" INT
+);
+
+-- match results table
+CREATE TABLE "match_results" (
+  "id" SERIAL PRIMARY KEY,
+  "user_one" INT REFERENCES "user",
+  "user_two" INT REFERENCES "user",
+  "user_one_score" INT,
+  "user_two_score" INT
 );
